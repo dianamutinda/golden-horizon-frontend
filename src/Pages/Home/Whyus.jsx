00519f428a77c445a7a231ecd46ab200596329e6
dianamutinda <jaydiana25@gmail.com>
2024-07-02 15:48:44 +0300

    creates the why us section

--- src/Pages/Home/Whyus.jsx
+++ src/Pages/Home/Whyus.jsx
@@ -1,17 +1,32 @@
 import "./Home.css";
 import whyus1 from "../../assets/whyus1.jpeg";
 const Whyus = () => {
-  // const images = [
-  //     id=1,
-  //     image= {whyus1}
-  // ]
-  // return(
-  //     <section>
-  //         <h1>why us</h1>
-  //         {images.map((images)
-  //     key= {images.id}
-  //     )}
-  //     </section>
-  // )
+  return(
+    <section>
+      <h1 className="whyus">why to choose us?</h1>
+      <p className="whyus-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla dolores quasi dolorem fugiat ea neque eveniet similique provident ex. Asperiores.</p>
+      <div className="images">
+        <div className="image1">
+          <img src={whyus1} alt="" />
+        </div>
+        <div className="image1">
+          <img src={whyus1} alt="" />
+        </div>
+        <div className="image1">
+          <img src={whyus1} alt="" />
+        </div>
+        <div className="image1">
+          <img src={whyus1} alt="" />
+        </div>
+        <div className="image1">
+          <img src={whyus1} alt="" />
+        </div>
+        <div className="image1">
+          <img src={whyus1} alt="" />
+        </div>
+      </div>
+    </section>
+  )
+  
 };
 export default Whyus;
