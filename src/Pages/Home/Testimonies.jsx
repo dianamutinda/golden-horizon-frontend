2ae8d247dac4edab6b67e12811633af7fd38358a
dianamutinda <jaydiana25@gmail.com>
2024-07-02 15:00:06 +0300

    creates the testimonies section

--- src/Pages/Home/Testimonies.jsx
+++ src/Pages/Home/Testimonies.jsx
@@ -1,8 +1,11 @@
 import { FaStar } from "react-icons/fa6";
+import profileImg from '../../assets/profile.jpeg'
+import './Home.css'
 const Testimonies = () => {
   return (
     <section>
-      <h1>Testimonies</h1>
+      <h1 className="test-title">Testimonies</h1>
+      <div className="container">
       <div className="test-container">
         <div className="stars">
           <p>
@@ -23,10 +26,68 @@ const Testimonies = () => {
         </div>
 
         <p>
-          The wise man therefore always holds in these matters to this principle
-          of selection
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus iste molestiae perspiciatis praesentium, minima eveniet labore.
         </p>
+        <div className="profile">
+       <img src={profileImg} alt="" />
+       </div>
       </div>
+
+      <div className="test-container">
+        <div className="stars">
+          <p>
+            <FaStar />
+          </p>
+          <p>
+            <FaStar />
+          </p>
+          <p>
+            <FaStar />
+          </p>
+          <p>
+            <FaStar />
+          </p>
+          <p>
+            <FaStar />
+          </p>
+        </div>
+
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus iste molestiae perspiciatis praesentium, minima eveniet labore.
+        </p>
+        <div className="profile">
+       <img src={profileImg} alt="" />
+       </div>
+      </div>
+
+      <div className="test-container">
+        <div className="stars">
+          <p>
+            <FaStar />
+          </p>
+          <p>
+            <FaStar />
+          </p>
+          <p>
+            <FaStar />
+          </p>
+          <p>
+            <FaStar />
+          </p>
+          <p>
+            <FaStar />
+          </p>
+        </div>
+
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus iste molestiae perspiciatis praesentium, minima eveniet labore.
+        </p>
+        <div className="profile">
+       <img src={profileImg} alt="" />
+       </div>
+      </div>
+      </div>
+    
     </section>
   );
 };
